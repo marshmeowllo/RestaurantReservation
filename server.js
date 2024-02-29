79feb3057a4c22f22220849580597e3a6026a3f9
Kananate Watewitee <kananate24@gmail.com>
2024-02-29 15:02:47 +0700

    reservation var

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 
 const restaurant = require('./routes/restaurant');
 const auth = require('./routes/auth');
-const appointments = require('./routes/appointments');
+const reservation  = require('./routes/reservation ');
 
 const limiter = ratelimit({
     windowMs: 10*60*1000, //10 mins
@@ -37,7 +37,7 @@ app.use(hpp());
 app.use(cors());
 app.use('/api/v1/restaurants', restaurant);
 app.use('/api/v1/auth', auth);
-app.use('/api/v1/appointments', appointments);
+app.use('/api/v1/reservation ', reservation );
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('server running in ',process.env.NODE_ENV, ' mode on port ', PORT));
