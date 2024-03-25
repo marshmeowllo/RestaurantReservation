763313bfd60c79aaad391f9ae92d6d20f4aa2314
marshmeowllo <6633012821@student.chula.ac.th>
2024-03-25 21:40:21 +0700

    Top Bar finish

---
+++ frontend/src/components/TopBar.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { BiMenu } from 'react-icons/bi';
+import Box from './marquee';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+import Marquee from './marquee';
+import { usePathname } from "next/navigation";
+
+interface NavBarProps {
+    userName: string;
+}
+
+export default function TopBar({ userName }: NavBarProps) {
+    const truncatedUserName = userName.length > 8 ? userName.slice(0, 5) + '...' : userName;
+
+    let pathname = usePathname() || "/";
+
+    return (
+        <nav className="max-w-1440 text-base flex flex-row z-30 wh-8 mt-9 ml-9 mr-9 mb-9">
+            <div className="w-[85%] mr-4 flex items-center bg-stone-800">
+                <Marquee />
+            </div>
+            <div className="w-[15%] flex items-center z-40 bg-stone-800">
+                <p className="pl-2 pr-2 pt-1 pb-1 text-stone-100">
+                    <span className="hidden sm:inline">{truncatedUserName}</span>
+                    <span className="sm:hidden sm:content-center">Menu</span>
+                </p>
+            </div>
+        </nav>
+    );
+}
--- frontend/src/components/marquee.tsx
+++ frontend/src/components/marquee.tsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 const marqueeVariants = {
   animate: {
-    x: [1440, -1440],
+    x: [1100, -1440],
     transition: {
       x: {
         repeat: Infinity,
@@ -17,17 +17,19 @@ const marqueeVariants = {
 
 const Marquee = () => {
   return (
+    <div className="marquee-container w-full h-full flex items-center justify-center overflow-hidden">      
     <motion.div
       className="absolute whitespace-nowrap"
       variants={marqueeVariants}
       animate="animate"
     >
-      <p className="pl-2 pr-2 pt-1 pb-1 text-white">
+      <p className="pl-2 pr-2 pt-1 pb-1 text-stone-100">
         Let's Work Together. Let's Work Together. Let's Work Together. Let's
         Work Together. Let's Work Together. Let's Work Together. Let's Work
         Together
       </p>
     </motion.div>
+    </div>
   );
 };
 
