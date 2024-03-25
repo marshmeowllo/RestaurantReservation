ff6ebeadb4349d2e4ce57373a3826140621f3124
marshmeowllo <6633012821@student.chula.ac.th>
2024-03-26 01:24:06 +0700

    create myTable

--- frontend/src/app/myTable/page.tsx
+++ frontend/src/app/myTable/page.tsx
@@ -1,5 +1,4 @@
 import getRestaurants from '@/libs/getRestaurants';
-import CardPanel from '@/components/CardPanel'
 import RestaurantCatalog from '@/components/RestaurantCatalog';
 import { Suspense } from 'react';
 import { LinearProgress } from '@mui/material';
@@ -8,6 +7,7 @@ export default function Home() {
   const restaurants = getRestaurants()
   return (
     <main className='w-[70%] text-center'>
+        <p className='text-4xl mb-16 text-left'>Your Reservation</p>
       <Suspense fallback={<p>Now Loading...<LinearProgress /></p>}>
         <RestaurantCatalog RestaurantsJson={restaurants} />
       </Suspense>
