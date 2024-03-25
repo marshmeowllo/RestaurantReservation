a44fdf15359ae4f42349549427edc83ee08e1c51
P-3142857 <144462891+P-3142857@users.noreply.github.com>
2024-03-26 00:54:25 +0900

    Update Sidebar

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,18 +1,30 @@
 import Link from 'next/link';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth';
 
-export default function SideBar() {
+export default async function SideBar() {
 
-    const listItems = [
+    const session = await getServerSession(authOptions)
+
+    
+    const preLogin = [
         { href: '/', label: 'Home' },
         { href: '/restaurant', label: 'Eatery' },
-        { href: '/api/auth/signin', label: 'Login' },
+        { href: '/api/auth/signin', label: 'Login' },    
+    ];
+    const postLogin = [
+        {href: '/', label: 'Home'},
+        {href: '/reserve', label: 'Reserve'},
+        {href: '/myTable', label: 'My Table'},
+        {href: '/api/auth/signout', label: "Logout"}
     ];
 
     return (
         <div className="width-[15%] ml-9 mr-9">
             <ul>
                 {
-                    listItems.map(({ href, label }) => {
+                    session?
+                    preLogin.map(({ href, label }) => {
                         return (
                             <li key={href} className="text-4xl border-b-4 border-stone-800 pb-9 mb-18">
                                 <Link href={href}>
@@ -21,6 +33,17 @@ export default function SideBar() {
                                     </p>
                                 </Link>
                             </li>
+                        );
+                    }):
+                    postLogin.map(({href,label})=>{
+                        return(
+                            <li key={href} className="text-4xl border-b-4 border-stone-800 pb-9 mb-18">
+                            <Link href={href}>
+                                <p className='text-stone-800'>
+                                    {label}
+                                </p>
+                            </Link>
+                        </li>
                         );
                     })
                 }
