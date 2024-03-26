78cdc9332e4a4644b4823dfbca59451695d30834
marshmeowllo <6633012821@student.chula.ac.th>
2024-03-26 20:26:09 +0700

    add map

--- frontend/src/app/(restaurantInfo)/restaurant/getonerestaurant/[rid]/page.tsx
+++ frontend/src/app/(restaurantInfo)/restaurant/getonerestaurant/[rid]/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import getRestaurant from '@/libs/getRestaurant';
+import Link from 'next/link';
 
 export default async function GetOne({ params }: { params: { rid: string } }) {
     const restaurantDetails = await getRestaurant(params.rid);
@@ -43,12 +44,19 @@ export default async function GetOne({ params }: { params: { rid: string } }) {
                     </div>
                     <div>
                         <p className="text-2xl mb-4	">Address</p>
-                        <p className="text-base mb-4 mr-4  border border-stone-800 py-2 px-4">
-                            <p>{restaurantDetails.data.address}</p>
-                            <p>District: {restaurantDetails.data.district}</p>
-                            <p>Postal Code: {restaurantDetails.data.postalcode}</p>
-                            <p>Region: {restaurantDetails.data.region}</p>
-                        </p>
+                        <Link href={restaurantDetails.data.map}
+                            className="block border p-2 text-center
+                            border-stone-800 relative overflow-hidden transition-transform duration-300 ease-in-out 
+                            hover:shadow-lg hover:shadow-stone-500/100 bg-stone-100 text-stone-800 transform 
+                            hover:-translate-x-1 hover:-translate-y-1">
+                            <div className="text-md mx-5 text-left">{restaurantDetails.data.name}
+                                <div>Address: {restaurantDetails.data.address}</div>
+                                <div>Province: {restaurantDetails.data.province}</div>
+                                <div>District {restaurantDetails.data.district}</div>
+                                <div>Zipcode: {restaurantDetails.data.postalcode}</div>
+                                <div>Tel: {restaurantDetails.data.tel}</div>
+                            </div>
+                        </Link>
                     </div>
 
                 </div>
