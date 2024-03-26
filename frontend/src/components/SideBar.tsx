79dba096b237f29bbb20921639e97c5f3115a85b
Kananate Watewitee <kananate24@gmail.com>
2024-03-26 17:19:32 +0700

    sideBar right

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -73,14 +73,55 @@ export async function RightSideBar({ RestaurantJson }: { RestaurantJson: Promise
 
 
     return (
-        <div className="width-[15%] mr-9">
-            <div className='width-[100%]]'>
-                <p className='text-base font-bold'>Ranking</p>
-                <ul>
-                    
-                </ul>
-            </div>
+        // <div className="width-[15%] mr-9">
+        //     <div className='width-[100%]]'>
+        //         {/* <p className='text-base font-bold'>Ranking</p>
+        //         <ul>
+        //         </ul> */}
+
+        // </div>
+        // </div>
+
+       
+        <div className="bg-neutral-100 w-72 h-96 p-6 flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
+          <h2 className="text-zinc-900 font-bold">Ranking</h2>
+          <hr className="border-zinc-900 w-28" />
+          <div className="flex items-center gap-2">
+            <span className="text-zinc-900">Eat Food</span>
+            <div className="bg-zinc-900 w-2.5 h-2.5 rounded-full"></div>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-zinc-900">Fast Food</span>
+            <div className="border-zinc-900 w-2.5 h-2.5 rounded-full"></div>
+          </div>
+          {/* Add more items here */}
         </div>
+        <hr className="border-zinc-900 w-52" />
+        <div className="flex flex-col gap-2">
+          <h2 className="text-zinc-900 font-bold">Category</h2>
+          <hr className="border-zinc-900 w-24" />
+          <div className="flex items-center gap-2">
+            <span className="text-zinc-900">Fine dining</span>
+            <div className="bg-zinc-900 w-2.5 h-2.5 rounded-full"></div>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-zinc-900">Fast food</span>
+            <div className="border-zinc-900 w-2.5 h-2.5 rounded-full"></div>
+          </div>
+          {/* Add more items here */}
+        </div>
+        <hr className="border-zinc-900 w-52" />
+        <div className="flex flex-col gap-2">
+          <h2 className="text-zinc-900 font-bold">Create blog</h2>
+          <hr className="border-zinc-900 w-20" />
+          <div className="bg-neutral-100 border-zinc-900 w-48 h-48"></div>
+        </div>
+      </div>
+      
+
+      
+ 
 
     );
 }
