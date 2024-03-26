e69f62b0cd9c4f6aac3d6dba614d57c66a773d20
marshmeowllo <6633012821@student.chula.ac.th>
2024-03-27 00:53:59 +0700

    add lastest files

--- swdevprac2-project-i-cut-my-hair/src/app/reserve/page.tsx
+++ swdevprac2-project-i-cut-my-hair/src/app/reserve/page.tsx
@@ -24,6 +24,7 @@ export default function booking() {
     const rid = urlParams.get('id')
     const rName = urlParams.get('name')
     const { data: session } = useSession()
+    
     //console.log(session?.user.token)
 
     const dispatch = useDispatch<AppDisptach>()
@@ -52,13 +53,13 @@ export default function booking() {
     const [fName, setfName] = useState<string>('')
     const [lname, setlName] = useState<string>('')
     const [cid, setCid] = useState<string>('')
+    const [hName, sethName] = useState<string | null>("Chula")
     const makeBooking = async () => {
         console.log(rid)
-        console.log(dayjs(bookDate).format("YYYY-MM-DD"))
-        if(!session || !session.user.token) return
-        console.log(dayjs(bookDate).format("YYYY-MM-DD") + "T" + dayjs(bookDate).format("HH:mm:ss")) //    "resvDate": "2024-12-24T22:50:00.000Z"
+        const utcTime = dayjs(bookDate).utc()
+        console.log(dayjs(bookDate).format("YYYY-MM-DD") + "T" + utcTime.format("HH:mm:ss")) //    "resvDate": "2024-12-24T22:50:00.000Z"
         if (rid && session) {
-            const response = await postReservation(rid, dayjs(bookDate).format("YYYY-MM-DD") + "T" + dayjs(bookDate).format("HH:mm:ss"), session.user.token)
+            const response = await postReservation(rid, dayjs(bookDate).format("YYYY-MM-DD") + "T" + utcTime.format("HH:mm:ss"), session.user.token)
         }
     }
 
@@ -72,13 +73,21 @@ export default function booking() {
                         <p className="text-4xl mb-16 font-bold">Reserve Table</p>
 
                         <div className="text-2xl mb-6">
-                            Resturant Name 
+                            {rName}
                         </div>
-                        <p className="text-2xl mb-6">{rName}</p>
+                        <p className="text-2xl mb-6">{session?.user.name}</p>
                         <p className="text-2xl mb-6">Date</p>
-                        <p className="text-4xl mb-4 inline-block border border-stone-800 p-2">
-                            <DateReserve onDateTimeChange={(value:Date)=>{setBookDate(value)}}/>
-                        </p>
+                        <p className="text-4xl mb-4 inline-block border border-stone-800 p-2">{now}</p>
+                    </div>
+                    <div className="flex flex-row">
+                        <div className="flex flex-col mr-9">
+                            <p className="text-2xl mb-6 text-left text-stone-800">Time</p>
+                            <p className="text-4xl mb-4 inline-block border border-stone-800 p-2">{currentTime}</p>
+                        </div>
+                        <div className="flex flex-col">
+                            <p className="text-2xl mb-6 text-left text-stone-800">Reserve by</p>
+                            <p className="text-4xl mb-6 inline-block border border-stone-800 p-2">{rid}</p>
+                        </div>
                     </div>
                 </div>
                 <div className="w-[50%]">
@@ -94,7 +103,7 @@ export default function booking() {
                 </Link>
                 <button className="text-base w-[80%] mb-4 mr-4 inline-block border border-stone-800 p-2 text-center relative overflow-hidden transition-transform duration-300 ease-in-out 
                         hover:shadow-lg hover:shadow-stone-500/100 bg-stone-100 hover:bg-stone-800 text-stone-800 hover:text-stone-100 transform 
-                        hover:-translate-x-1 hover:-translate-y-1" onClick={makeBooking}>Reserve Now!</button>
+                        hover:-translate-x-1 hover:-translate-y-1">Reserve Now!</button>
 
             </div>
         </main>
