74ae883cd2d04e4d50f0987f30373ec5ee401495
marshmeowllo <6633012821@student.chula.ac.th>
2024-03-26 10:35:56 +0700

    add reservation

---
+++ frontend/src/app/payment/page.tsx
@@ -0,0 +1,42 @@
+export default function payment() {
+    return (
+        <div className="w-[70%]">
+            <h1 className="text-4xl	font-bold mb-16">Checkout</h1>
+            <div className="flex flex-row">
+                <div className="w-[50%]">
+                    <p className="text-2xl mb-4	">Restaurant_Name</p>
+                    <p className="text-2xl mb-4	">Date</p>
+                    <p className="text-4xl mb-4 inline-block border border-stone-800 p-2">Mon 1 Jan 1999</p>
+                    <div>
+                        <div>
+                            <p className="text-2xl mb-4	">Time</p>
+                            <p className="text-4xl mb-4 inline-block border border-stone-800 p-2">12:00</p>
+                        </div>
+                        <div>
+                            <p className="text-2xl mb-4	">Payment Methods</p>
+                            <p className="text-4xl mb-4 inline-block border border-stone-800 p-2">credit</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-[50%]">
+                    <p className="text-2xl mb-9">Payment</p>
+                        <div className="flex flex-row justify-between mb-9	">
+                            <p className="text-base">Amount</p>
+                            <p className="text-base">5,000 THB</p>
+                        </div>
+                        <div className="flex flex-row justify-between mb-9	">
+                            <p className="text-base">Mr.xxxx xxxxx</p>
+                            <p className="text-base">date and time now</p>
+                        </div>
+                    <p className="text-2xl mb-4">total</p>
+                    <p className="text-4xl mb-4 inline-block border border-stone-800 p-2">THB: 5,000.00</p>
+                </div>
+            </div>
+            <div className="flex flex-row">
+            <p className="text-base w-[20%] mb-4 mr-4 inline-block border border-stone-800 p-2 text-center">Back</p>
+            <p className="text-base w-[60%] mb-4 mr-4 inline-block border border-stone-800 p-2 text-center">Pay It!</p>
+            <p className="text-base w-[20%] mb-4 inline-block border border-stone-800 p-2 text-center">Cancel</p>
+            </div>
+        </div>
+    );
+}
